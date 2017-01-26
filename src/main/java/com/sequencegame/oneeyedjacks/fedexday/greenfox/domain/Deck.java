83e20d38ec97c3fdc2549b33b3878e2fa065f320
radaladar <bbiczok@gmail.com>
2017-01-26 14:04:25 +0100

    deck class added

---
+++ src/main/java/com/sequencegame/oneeyedjacks/fedexday/greenfox/domain/Deck.java
@@ -0,0 +1,8 @@
+/// Created by BB on 2017-01-26.
+package com.sequencegame.oneeyedjacks.fedexday.greenfox.domain;
+
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> deckOrder;
+}
