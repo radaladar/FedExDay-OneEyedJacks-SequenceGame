7e707d66b97c4a8817bda55943936ba863ffc8ba
radaladar <bbiczok@gmail.com>
2017-01-26 13:35:32 +0100

    initial commit of fedexday project

---
+++ src/main/java/com/sequencegame/oneeyedjacks/fedexday/greenfox/SequencegameApplication.java
@@ -0,0 +1,12 @@
+package com.sequencegame.oneeyedjacks.fedexday.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequencegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SequencegameApplication.class, args);
+	}
+}
