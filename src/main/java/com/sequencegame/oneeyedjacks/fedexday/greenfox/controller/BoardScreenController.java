9500b550d436f9b051b3da10e840631edaaf438e
radaladar <bbiczok@gmail.com>
2017-01-27 12:16:07 +0100

    stuff mostly works now yeeeeeeaaaah

---
+++ src/main/java/com/sequencegame/oneeyedjacks/fedexday/greenfox/controller/BoardScreenController.java
@@ -0,0 +1,13 @@
+/// Created by BB on 2017-01-27.
+package com.sequencegame.oneeyedjacks.fedexday.greenfox.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardScreenController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}
--- src/main/java/com/sequencegame/oneeyedjacks/fedexday/greenfox/controller/WelcomeScreenController.java
+++ src/main/java/com/sequencegame/oneeyedjacks/fedexday/greenfox/controller/WelcomeScreenController.java
@@ -1,25 +1,40 @@
 package com.sequencegame.oneeyedjacks.fedexday.greenfox.controller;
 
-import com.sequencegame.oneeyedjacks.fedexday.greenfox.SequencegameApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import org.springframework.stereotype.Component;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Button;
 
 
 /**
  * Created by ${SzilviaB} on 2017. 01. 26..
  */
-@Component
-public class WelcomeScreenController {
-
-    private SequencegameApplication main;
+public class WelcomeScreenController implements Initializable {
 
     @FXML
-    public void getBoardScreen() throws IOException {
-            main.showMainScreen();
+    public Button buttonplayer2;
+
+    @FXML
+    public void handleButtonClick(ActionEvent event) throws IOException {
+        System.out.println("click!");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Board.fxml"));
+        Parent board = fxmlLoader.load();
+        Scene boardscene = new Scene(board);
+        Stage board_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        board_stage.hide();
+        board_stage.setScene(boardscene);
+        board_stage.show();
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
 }
-
-
